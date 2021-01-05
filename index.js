7f5a1af56b24b74ec76119cdb0756e16d05cddd2
choieunji <ghk784@dgu.ac.kr>
2021-01-05 20:06:35 +0900

    초기세팅

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "eunji", age = 24, gender = "male";
+const sayEUN = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayEUN(name, age, gender);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "eunji",
+    age = 24,
+    gender = "male";
+
+const sayEUN = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayEUN(name, age, gender);
+
+export {};
